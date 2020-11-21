8cf3423225e828cf34e7475e3e828fa22c30b82a
Danny Gilbert <spydercapriani@gmail.com>
2020-11-21 16:17:16 -0600

    Initial Commit

--- Package.swift
+++ Package.swift
@@ -17,18 +17,12 @@ let package = Package(
         ),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+
     ],
     targets: [
         .target(
             name: "Analytics",
-            dependencies: []),
-        .testTarget(
-            name: "AnalyticsTests",
-            dependencies: [
-                "Analytics"
-            ]
+            dependencies: []
         ),
     ]
 )
